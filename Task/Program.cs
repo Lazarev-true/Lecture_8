10ee59cbf250e70b184235d9e5db3196f9932bca
Konstantin <klik@mail.ru>
2022-09-20 18:50:47 +0300

    Треугольник

---
+++ Task/Program.cs
@@ -0,0 +1,24 @@
+﻿class PascalTriangle
+{
+  static void Main(string[] args)
+  {
+    int n = 7;
+ 
+    for (int y = 0; y < n; y++)
+    {
+      int c = 1;
+      for (int q = 0; q < n - y; q++)
+      {
+        System.Console.Write("   ");
+      }
+ 
+      for (int x = 0; x <= y; x++)
+      {
+        Console.Write("   {0:D} ", c);
+        c = c * (y - x) / (x + 1);
+      }
+      Console.WriteLine();
+    }
+    Console.WriteLine();
+    }
+}
